c31c123bb8177fa72097ba15145c86f64350a32d
khoanguyendangthai <156320922+khoanguyendangthai@users.noreply.github.com>
2024-04-23 12:01:19 +0700

    Add files via upload

---
+++ demo (1)/demo/src/main/java/MMT/demo/P2PFileSharingApplication.java
@@ -0,0 +1,13 @@
+package MMT.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PFileSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2PFileSharingApplication.class, args);
+	}
+
+}
